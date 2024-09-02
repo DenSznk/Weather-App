82bec4b06321179be1ab804655888c1529526631
Dzianis <d.sazonenka@andersenlab.com>
2024-09-02 10:47:29 +0400

    add gesture

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/Home/Home';
 import Users from './screens/Users/Users';
 import Reanim from './screens/Reanim/Reanim';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-
+    <GestureHandlerRootView style={{flex: 1}}>
+      {' '}
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
@@ -18,6 +20,7 @@ function App(): React.JSX.Element {
           <Stack.Screen name="Reanim" component={Reanim} />
         </Stack.Navigator>
       </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 export default App;
